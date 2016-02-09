8fbb44b7eaaba6b2f3561355d8c7f6aaa7468dc1
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-02-09 17:11:37 +0300

    add simple test for silent move execution

--- src/model/spec/move-execution.spec.js
+++ src/model/spec/move-execution.spec.js
@@ -4,7 +4,25 @@ var assert = require('chai').assert,
   Chess = require('../chess');
 
 describe('Move execution', function () {
-  describe('Pawn silent move', function () {
-    var chess = Chess.createStartPosition();
+  describe('silent move', function () {
+    var chess;
+
+    beforeEach(function () {
+      var move;
+
+      chess = Chess.createStartPosition();
+      move = chess.createMove('e2-e3');
+      move.execute();
+    });
+
+    it('source square should be empty after move execution', function () {
+      var sourceSquare = chess.getSquareByName('e2');
+      assert.isTrue(sourceSquare.isEmpty());
+    });
+
+    it('target square should be occupied', function () {
+      var targetSquare = chess.getSquareByName('e3');
+      assert.isTrue(targetSquare.isOccupied());
+    });
   });
 });
