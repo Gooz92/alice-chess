566f5bb9a43e2e8b0a6af09907be230b0a72b08b
Gooz92 <kurguzenkov@gmail.com>
2016-02-22 23:50:43 +0200

    add piece characters and styles for them

--- src/browser/app.js
+++ src/browser/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var Chess = require('../model/chess'),
+  pieceCharacters = require('./utils/piece-characters'),
   createTable = require('./utils/create-table');
 
 var chess = Chess.createStartPosition();
@@ -12,10 +13,22 @@ var table = createTable({
     className: 'chess-board'
   },
   getCellOptions: function (rowIndex, columnIndex) {
-    var className = (rowIndex + columnIndex) % 2 === 0 ? 'light' : 'dark';
+    var className = (rowIndex + columnIndex) % 2 === 0 ? 'light' : 'dark',
+      innerHTML = '';
+    
+    if (rowIndex === 1) {
+      innerHTML = pieceCharacters.p; 
+    } else if (rowIndex === 6) {
+      innerHTML = pieceCharacters.P;
+    } else if (rowIndex === 0) {
+      innerHTML = pieceCharacters['rnbqkbnr'.charAt(columnIndex)];
+    } else if (rowIndex === 7) {
+      innerHTML = pieceCharacters['RNBQKBNR'.charAt(columnIndex)];
+    }
 
     return {
-      className: className
+      className: className,
+      innerHTML: innerHTML
     };
   }
 });
