d38f3ef70cc51e6ef31dbd754ead0474c62b5c40
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-04-21 13:06:34 +0300

    add castling move exection rough implementation

--- src/model/pieces/king.js
+++ src/model/pieces/king.js
@@ -1,16 +1,43 @@
 'use strict';
 
 var boardUtils = require('../../utils/chess-utils/board-utils'),
-  Move = require('../move');
+  Move = require('../move'),
+  squares = require('../../utils/chess-utils/squares');
 
-var offsets = [16, -16, 1, -1, 15, 17, -15, -17],
-  castlingOffsets = [
-    -2, // queen-side (long)
-    2 // king-side (short)
-  ];
+var offsets = [16, -16, 1, -1, 15, 17, -15, -17];
 
 module.exports = {
   token: 'k',
+
+  createShortCastlingMove: function () {
+    var self = this,
+      rookSquareIndex = this.color.isWhite() ? squares.h1 : squares.h8,
+      rook = this.square.chess.squares[rookSquareIndex].piece,
+      rookTargetSquare = this.square.chess.squares[rookSquareIndex - 2],
+      kingTargetSquare = this.square.chess.squares[this.square.index + 2];
+
+    return {
+      make: function () {
+        self.moveTo(kingTargetSquare);
+        rook.moveTo(rookTargetSquare);
+      }
+    };
+  },
+
+  createLongCastlingMove: function () {
+    var self = this,
+      rookSquareIndex = this.color.isWhite() ? squares.a1 : squares.a8,
+      rook = this.square.chess.squares[rookSquareIndex].piece,
+      rookTargetSquare = this.square.chess.squares[rookSquareIndex + 3],
+      kingTargetSquare = this.square.chess.squares[this.square.index - 2];
+
+    return {
+      make: function () {
+        self.moveTo(kingTargetSquare);
+        rook.moveTo(rookTargetSquare);
+      }
+    };
+  },
 
   forEachMove: function (callback) {
     var self = this,
