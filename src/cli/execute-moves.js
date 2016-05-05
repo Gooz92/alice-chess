ba78a7e7a104d0689ac74c0c3937f2a54a7713a0
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-05-05 18:42:14 +0300

    add simple cli utility for move execution

---
+++ src/cli/execute-moves.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* jshint esnext: true */
+
+var Chess = require('../model/chess'),
+  chess = Chess.createStartPosition(),
+  moves = process.argv.splice(2);
+
+moves.forEach(move => chess.move(move));
+
+console.log(chess.toASCII());
+
+console.log(chess.generateMoveNames().join(' '));
