2c1c33c1339c24ec64b284dd710817e05154e5c8
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-05-19 18:05:29 +0300

    add first move parameters in cli for perft generation

--- src/cli/perft.js
+++ src/cli/perft.js
@@ -3,9 +3,23 @@
 var Chess = require('../model/chess');
 
 var chess = Chess.createStartPosition(),
-  initialDepth = process.argv[2];
+  initialDepth = process.argv[2],
+  moves = process.argv.splice(3);
 
 initialDepth = parseInt(initialDepth);
+
+function executeMoves(moves) {
+  var index;
+
+  for (index = 0; index < moves.length; index++) {
+    if (!chess.move(moves[index])) {
+      console.log("Invalid move: '" + moves[index] + "'");
+      return false;
+    }
+  }
+
+  return true;
+}
 
 function perft(depth) {
   var nodes = 0, moves;
@@ -32,6 +46,6 @@ function perft(depth) {
   return nodes;
 }
 
-console.time('time');
-console.log(perft(initialDepth));
-console.timeEnd('time');
+if (executeMoves(moves)) {
+  perft(initialDepth);
+}
