8ce8b82c3a598de02d6416d1ab880b3e67346633
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-06-21 18:10:54 +0300

    imporve perft benchmarking

--- src/cli/perft-benchmark.js
+++ src/cli/perft-benchmark.js
@@ -1,62 +1,10 @@
 'use strict';
 
-var Chess = require('../model/chess');
+var Chess = require('../model/chess'),
+  averageTime = require('../utils/common-utils/benchmark-utils').averageTime;
 
-function executeMoves(moves) {
-  var index;
+var chess = Chess.createStartPosition();
 
-  for (index = 0; index < moves.length; index++) {
-    if (!chess.move(moves[index])) {
-      console.log("Invalid move: '" + moves[index] + "'");
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function perft(depth) {
-  var nodes = 0, moves;
-
-  if (depth === 0) {
-    return 1;
-  }
-
-  moves = chess.generateMoves(true);
-
-  moves.forEach(function (move) {
-    var subresult;
-
-    move.make();
-    if (!chess.isOpponentInCheck()) {
-      subresult = perft(depth - 1);
-      nodes += subresult;
-    }
-    move.unMake();
-  });
-
-  return nodes;
-}
-
-var initialDepth = process.argv[2],
-  moves = process.argv.splice(3),
-  chess;
-
-initialDepth = parseInt(initialDepth);
-
-var times = 10, time, executionTimes = [];
-
-while (times--) {
-   chess = Chess.createStartPosition();
-  if (executeMoves(moves)) {
-    time = Date.now();
-    perft(initialDepth);
-    executionTimes.push(Date.now() - time);
-  } else {
-    break;
-  }
-}
-
-console.log(executionTimes);
-console.log(executionTimes.reduce((a, b) => a + b) / executionTimes.length);
-
+console.log(averageTime(function () {
+  chess.traverse(4);
+}, 10));
