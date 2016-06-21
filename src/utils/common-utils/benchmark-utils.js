4b9b36baddae07f61c2e58c78e297a4fd0a2634b
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-06-21 18:10:22 +0300

    add simple utils for profiling

---
+++ src/utils/common-utils/benchmark-utils.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var benchmarkUtils = module.exports = {
+  time: function (fn) {
+    var time = Date.now();
+    fn();
+    return Date.now() - time;
+  },
+
+  averageTime: function (fn, times) {
+    var timesLeft = times, result = 0, time;
+
+    while (--timesLeft > 0) {
+      time = Date.now();
+      fn();
+      result += Date.now() - time;
+    }
+
+    return result;
+  }
+};
