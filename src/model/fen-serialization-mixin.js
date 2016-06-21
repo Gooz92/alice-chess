53273325faed7074265470427b67c95b9680789f
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-06-21 15:01:12 +0300

    add fen serialization functions

--- src/model/fen-serialization-mixin.js
+++ src/model/fen-serialization-mixin.js
@@ -3,7 +3,72 @@
 var castlingRightsUtils = require('../utils/chess-utils/castling-rights-utils');
 
 module.exports = {
+  generateFenRank: function (rankIndex) {
+    var rank = '',
+      squareIndex = rankIndex * 16,
+      endSquareIndex = squareIndex + 8,
+      emptySquaresCount = 0,
+      square;
+
+    while (squareIndex < endSquareIndex) {
+      square = this.squares[squareIndex];
+
+      if (square.isEmpty()) {
+        ++emptySquaresCount;
+      } else {
+        if (emptySquaresCount > 0) {
+          rank += emptySquaresCount;
+          emptySquaresCount = 0;
+        }
+        rank += square.piece.getFenToken();
+      }
+
+      ++squareIndex;
+    }
+
+    if (emptySquaresCount > 0) {
+      rank += emptySquaresCount;
+    }
+
+    return rank;
+  },
+
+  generatePiecePlacement: function () {
+    var piecePlacement = [],
+      rank, rankIndex;
+
+    for (rankIndex = 7; rankIndex >= 0; rankIndex--) {
+      rank = this.generateFenRank(rankIndex);
+      piecePlacement.push(rank);
+    }
+
+    return piecePlacement.join('/');
+  },
+
+  generateEnPassantTargetSquare: function () {
+    if (this.enPassantTargetSquare) {
+      return this.enPassantTargetSquare.name;
+    }
+
+    return '-';
+  },
+
   generateFenCastlingAvailability: function () {
     return castlingRightsUtils.toFenField(this.castlingRights);
+  },
+
+  generateFullmoveNumber: function () {
+    return Math.ceil((this.history.length + 1) / 2);
+  },
+
+  generateFen: function () {
+    return [
+      this.generatePiecePlacement(),
+      this.activeColor.token,
+      this.generateFenCastlingAvailability(),
+      this.generateEnPassantTargetSquare(),
+      this.halfmoveClock,
+      this.generateFullmoveNumber()
+    ].join(' ');
   }
 };
--- src/model/spec/fen-serialization-mixin.spec.js
+++ src/model/spec/fen-serialization-mixin.spec.js
@@ -3,10 +3,33 @@
 var fenSerializationMixin = require('../fen-serialization-mixin'),
   castlingUtils = require('../../utils/chess-utils/castling-rights-utils'),
   assert = require('chai').assert,
-  sinon = require('sinon');
+  sinon = require('sinon'),
+  Chess = require('../chess');
 
 describe('fenSerializationMixin', function () {
   describe('generateFenCastlingAvailability()', function () {
     it('toFenField called with chess.castlingRights');
   });
+
+  describe('.generateFenRank()', function () {
+    Chess.prototype.generateFenRank = fenSerializationMixin.generateFenRank;
+
+    it("return '8' for empty rank", function () {
+      var chess = new Chess();
+      assert.strictEqual(chess.generateFenRank(0), '8');
+    });
+
+    it("return 'rnbqkbnr' for eighth rank on start position", function () {
+      var chess = Chess.createStartPosition();
+      assert.strictEqual(chess.generateFenRank(7), 'rnbqkbnr');
+    });
+
+    it("return '4P3' for fourth rank after king's pawn move", function () {
+      var chess = Chess.createStartPosition();
+      chess.move('e4');
+      assert.strictEqual(chess.generateFenRank(3), '4P3');
+    });
+  });
+
+  // describe('.generatePiecePlacement()');
 });
