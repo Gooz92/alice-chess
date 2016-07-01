f5490abb7227ff1d96d80089211f71f093c850f9
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-07-01 13:36:21 +0300

    improvement of move parsing prototype

--- src/utils/chess-utils/parse-move.js
+++ src/utils/chess-utils/parse-move.js
@@ -2,8 +2,8 @@
 
 var pattern = /^([RNBQK])?([a-h])?([1-8])?(x)?([a-h][1-8])(\=([RNBQ]))?(\+#)?$/,
   pieceTokenIndex = 1,
-  disambiguationFileIndex = 2, //
-  disambiguationRankIndex = 3,
+  fromFileIndex = 2,
+  fromRankIndex = 3,
   captureFlagIndex = 4,
   destinationSquareIndex = 5,
   promotedPieceIndex = 7;
@@ -15,7 +15,7 @@ function parseMoveName(moveName, position) {
     capture = match[captureFlagIndex] ? true : false,
     promotion = match[promotedPieceIndex],
     move = {
-       piece: pieceToken || 'p',
+      piece: pieceToken || 'P',
       to: destinationSquareName,
       capture: capture
     };
--- src/utils/chess-utils/spec/parse-move.spec.js
+++ src/utils/chess-utils/spec/parse-move.spec.js
@@ -4,15 +4,28 @@ var parseMove = require('../parse-move'),
   assert = require('chai').assert;
 
 describe('parseMove()', function () {
-  it("return object with piece = 'p' for pawn moves", function () {
+  it("return object with piece = 'P' for pawn moves", function () {
     var move = parseMove('e4');
-    assert.strictEqual(move.piece, 'p');
-    move = parseMove('e2e4');
-    move = parseMove
+    assert.strictEqual(move.piece, 'P');
   });
 
   it('return object with destination square for pawn moves', function () {
     var move = parseMove('e5');
     assert.strictEqual(move.to, 'e5');
+  });
+
+  it("return object with piece = 'N' for knight moves", function () {
+    var move = parseMove('Nc3');
+    assert.strictEqual(move.piece, 'N');
+  });
+
+  it("return object with destination square for knight moves", function () {
+    var move = parseMove('Nf3');
+    assert.strictEqual(move.to, 'f3');
+  });
+
+  it("return object with true capture flag for captures", function () {
+    var move = parseMove('Nxe5');
+    assert.isTrue(true);
   });
 });
