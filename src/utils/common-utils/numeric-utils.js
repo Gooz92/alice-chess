446650b55162771b25a8016f75bfc7ebe206ad99
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-07-07 14:57:38 +0300

    add methods for number formatting

--- src/utils/common-utils/numeric-utils.js
+++ src/utils/common-utils/numeric-utils.js
@@ -1,9 +1,37 @@
 'use strict';
 
-var POSITIVE_INTEGER_PATTERN = /^\d+$/;
+var NON_NEGATIVE_INTEGER_PATTERN = /^\d+$/;
 
-module.exports = {
+var numericUtils = module.exports = {
   isNonNegativeInteger: function (arg) {
-    return POSITIVE_INTEGER_PATTERN.test(arg);
+    return NON_NEGATIVE_INTEGER_PATTERN.test(arg);
+  },
+
+  splitInThousands: function (number) {
+    var thousands = [], currentIndex, nextIndex;
+
+    number = number.toString();
+
+    currentIndex = number.length;
+
+    while (currentIndex > 2) {
+      nextIndex = currentIndex - 3;
+      thousands.unshift(number.slice(nextIndex, currentIndex));
+      currentIndex = nextIndex;
+    }
+
+    if (currentIndex !== 0) {
+      thousands.unshift(number.slice(0, currentIndex))
+    }
+
+    return thousands;
+  },
+
+  formatThousands: function (number, separator) {
+    var thousands = numericUtils.splitInThousands(number);
+
+    separator = separator || ' ';
+
+    return thousands.join(separator);
   }
 };
--- src/utils/common-utils/spec/numeric-utils.spec.js
+++ src/utils/common-utils/spec/numeric-utils.spec.js
@@ -21,4 +21,35 @@ describe('numericUtils', function () {
       assert.isFalse(isNonNegativeInteger(-6));
     });
   });
+
+  describe('.splitInThousands()', function () {
+    var splitInThousands = numericUtils.splitInThousands;
+    it('split number in thousands groups', function () {
+      var number = 123456789;
+      assert.deepEqual(splitInThousands(number), ['123', '456', '789']);
+    });
+
+    it('first thousand group may content two digits', function () {
+      assert.deepEqual(splitInThousands(12345), ['12', '345']);
+    });
+
+    it('first thousand group may content one digit', function () {
+       assert.deepEqual(splitInThousands(1234567), ['1', '234', '567']);
+    });
+
+    it('return array with same string if string.length < 4', function () {
+      assert.deepEqual(splitInThousands(12), ['12']);
+    });
+  });
+
+  describe('.formatThousands()', function () {
+    var formatThousands = numericUtils.formatThousands;
+    it('split thousands use space as default separator', function () {
+      assert.deepEqual(formatThousands(1000000), '1 000 000');
+    });
+
+    it('may take custom separator as argument', function () {
+      assert.deepEqual(formatThousands(1000, ','), '1,000');
+    })
+  });
 });
