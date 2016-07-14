9396e554e15efb4e06cafe66921bdd5cc1456d41
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-07-14 14:27:15 +0300

    add new fen parser implementation

---
+++ src/utils/fen-parser/fen-parser.js
@@ -0,0 +1,96 @@
+'use strict';
+
+var PiecePlacementParser = require('./piece-placement-parser'),
+  CastlingRightsParser = require('./castling-rights-parser'),
+  fenUtils = require('../chess-utils/fen-utils'),
+  objectUtils = require('../common-utils/object-utils'),
+  langFns = require('../common-utils/lang-fns');
+
+var defaultHandlers = {
+  onStart: langFns.noop,
+
+  /* for piece placement parsing PiecePlacementParser used */
+
+  onActiveColor: langFns.noop,
+  onWhiteActiveColor: langFns.noop,
+  onBlackActiveColor: langFns.noop,
+
+  /* for castling rights parsing CastlingRightsParser used */
+
+  onEnPassantTargetSquare: langFns.noop,
+  onHalfmoveClock: langFns.noop,
+  onFullmoveNumber: langFns.noop,
+  onEnd: langFns.noop
+};
+
+var handlerNames = Object.keys(defaultHandlers);
+
+var fieldParseFunctions = [
+  function parsePiecePlacement (piecePlacement, data) {
+    this.handlers.piecePlacementParser.parse(piecePlacement, data);
+  },
+
+  function parseActiveColor (activeColor, data) {
+    if (!fenUtils.isColorToken(activeColor)) {
+      throw new Error();
+    }
+
+    this.handlers.onActiveColor.call(data, activeColor);
+
+    if (activeColor === 'w') {
+      this.handlers.onWhiteActiveColor.call(data);
+    } else {
+      this.handlers.onBlackActiveColor.call(data);
+    }
+  },
+
+  function parseCastlingRights (castlingRights, data) {
+    this.castlingRightsParser.parse(castlingRights, data);
+  },
+
+  function parseEnPassantTargetSquare (enPassantTargetSquare, data) {
+    if (!fenUtils.isEnPassantSquareToken(enPassantTargetSquare)) {
+      throw new Error();
+    }
+
+    this.handlers.onEnPassantTargetSquare.call(data, enPassantTargetSquare);
+  },
+
+  function parseHalfmoveClock (halfMoveClock, data) {
+    halfMoveClock = parseInt(halfMoveClock);
+    this.handlers.onHalfmoveClock.call(data, halfMoveClock);
+  },
+
+  function parseFullmoveNumber(fullmoveNumber, data) {
+    fullmoveNumber = parseInt(fullmoveNumber);
+    this.handlers.onFullmoveNumber.call(data, fullmoveNumber);
+  }
+];
+
+function FenParser(handlers) {
+  this.piecePlacementParser = new PiecePlacementParser(handlers.piecePlacement);
+  this.castlingRightsParser = new CastlingRightsParser(handlers.castlingRights);
+
+  handlers = objectUtils.pick(handlers, handlerNames);
+  handlers = objectUtils.defaults(handlers, defaultHandlers);
+
+  this.handlers = handlers;
+}
+
+FenParser.prototype.parse = function (fen) {
+  var data = {},
+    fenParser = this,
+    fields;
+
+  this.handlers.onStart.call(data, fen);
+
+  fields = fen.split('');
+
+  fields.forEach(function (field, index) {
+    fieldParseFunctions[index].call(fenParser, field, data);
+  });
+
+  return this.handlers.onEnd.call(data, fen);
+};
+
+module.exports = FenParser;
