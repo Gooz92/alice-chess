c5c877d2ef52e92a614f12f822b1b78ccf334d34
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-07-15 13:04:24 +0300

    add specs for castling rights parsing

--- src/utils/fen-parser/spec/castling-rights-parser.handlers.spec.js
+++ src/utils/fen-parser/spec/castling-rights-parser.handlers.spec.js
@@ -1,11 +1,37 @@
 'use strict';
 
-var CastlingRightsParser = require('../castling-rights-parser');
+var CastlingRightsParser = require('../castling-rights-parser'),
+  assert = require('chai').assert,
+  sinon = require('sinon');
 
 describe('CastlingRightsParser', function () {
   describe('#parse()', function () {
-    describe('onStart');
-    describe('onCastlingRightsToken');
-    describe('onEnd');
+    describe('onStart', function () {
+      it('take not parsed castling rights as argument', function () {
+        var onStart = sinon.spy(),
+          parser = new CastlingRightsParser({
+            onStart: onStart
+          }),
+          castlingRights = 'Kq';
+
+        parser.parse(castlingRights);
+
+        assert.isTrue(onStart.calledWith(castlingRights));
+      });
+    });
+    describe('onCastlingRightsToken', function () {
+      it('called for every castling right token', function () {
+        var onCastlingRightsToken = sinon.spy(),
+          parser = new CastlingRightsParser({
+            onCastlingRightsToken: onCastlingRightsToken
+          }),
+          castlingRights = 'Qk';
+
+        parser.parse(castlingRights);
+
+        assert.deepEqual(onCastlingRightsToken.firstCall.args, ['Q']);
+        assert.deepEqual(onCastlingRightsToken.secondCall.args, ['k']);
+      });
+    });
   });
 });
