50cc0df279acc2fafddbe64a1845753574ddf17c
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-07-15 14:43:49 +0300

    add spec for errors throwing in piece-placement-parser

--- src/utils/fen-parser/spec/piece-placement-parser.errors.spec.js
+++ src/utils/fen-parser/spec/piece-placement-parser.errors.spec.js
@@ -5,6 +5,13 @@ var PiecePlacementParser = require('../piece-placement-parser'),
 
 describe('PiecePlacementParser', function () {
   describe('#parse()', function () {
-    it('throw error if ranks number not equal to 8');
+    it('throw error if ranks number not equal to 8', function () {
+      var parser = new PiecePlacementParser(),
+        invalidPiecePlacement = 'rnbqkbnr/pppppppp/8/8/4P3/PPPP1PPP/RNBQKBNR';
+
+      assert.throws(function () {
+        parser.parse(invalidPiecePlacement);
+      });
+    });
   });
 });
