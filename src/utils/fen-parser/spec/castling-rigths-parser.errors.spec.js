74b8db7dade3f1cca6bd30b42fa1b4ea3e730baf
Anton Kurguzenkov <anton.kurhuzenkav@intexsoft.com>
2016-07-15 13:16:23 +0300

    add specs for castling rihgts parser

--- src/utils/fen-parser/spec/castling-rigths-parser.errors.spec.js
+++ src/utils/fen-parser/spec/castling-rigths-parser.errors.spec.js
@@ -1,11 +1,37 @@
 'use strict';
 
-var CastlingRightsParser = require('../castling-rights-parser');
+var CastlingRightsParser = require('../castling-rights-parser'),
+  assert = require('chai').assert;
 
 describe('CastlingRightsParser', function () {
   describe('#parse()', function () {
-    it('throw error for invalid castling rights token');
-    it('throw error for repeated castling token');
-    it('throw error for wrong castling token order');
+    var parser;
+    beforeEach(function () {
+      parser = new CastlingRightsParser();
+    });
+
+    it('throw error for invalid castling rights token', function () {
+      var invalidCastlingRights = 'Qks';
+      // TODO add checking for error message
+      assert.throws(function () {
+        parser.parse(invalidCastlingRights);
+      });
+    });
+
+    it('throw error for repeated castling token', function () {
+      var invalidCastlingRights = 'KK';
+
+      assert.throws(function () {
+        parser.parse(invalidCastlingRights);
+      });
+    });
+
+    it('throw error for wrong castling token order', function () {
+      var invalidCastlingRights = 'qK';
+
+      assert.throws(function () {
+        parser.parse(invalidCastlingRights);
+      });
+    });
   });
 });
