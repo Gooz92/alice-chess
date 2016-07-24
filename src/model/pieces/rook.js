a5d4adaee7641282f2046a0ca2399d6d7894a10a
Gooz92 <kurguzenkov@gmail.com>
2016-07-24 12:27:48 +0300

    use new rook capture model for move generation

--- src/model/pieces/rook.js
+++ src/model/pieces/rook.js
@@ -1,6 +1,41 @@
 'use strict';
 
+var boardUtils = require('../../utils/chess-utils/board-utils'),
+  Move = require('../move');
+
 module.exports = {
   token: 'r',
-  offsets: [-1, 1, 16, -16]
+  offsets: [-1, 1, 16, -16],
+
+  forEachMove: function (callback, pseudoLegal) {
+    var self = this;
+
+    this.offsets.forEach(function (offset) {
+      var targetSquareIndex = self.square.index + offset,
+        targetSquare, move;
+
+      while (boardUtils.isSquareOnBoard(targetSquareIndex)) {
+        targetSquare = self.square.chess.squares[targetSquareIndex];
+
+        if (targetSquare.isOccupied()) {
+          if (targetSquare.piece.color !== self.color) {
+            move = Move.createRookCapture(self.square, targetSquare);
+            if (pseudoLegal || !targetSquare.chess.isInCheckAfter(move)) {
+              callback.call(self, move);
+            }
+          }
+          return;
+        }
+
+        move = Move.createSilentMove(self.square, targetSquare);
+
+        if (pseudoLegal || !targetSquare.chess.isInCheckAfter(move)) {
+          callback.call(self, move);
+        }
+
+        targetSquareIndex += offset;
+      }
+
+    });
+  }
 };
