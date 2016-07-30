016f2214e5160779581f9158a60811c6735b7b96
Gooz92 <kurguzenkov@gmail.com>
2016-07-30 12:24:54 +0300

    add formattin to kiwipete position

--- src/cli/kiwipete-perft.js
+++ src/cli/kiwipete-perft.js
@@ -10,32 +10,14 @@ var chess = new Chess(),
 initialDepth = parseInt(initialDepth);
 
 chess.place({
-  a8: 'r',  e8: 'k',  h8: 'r',
-  a7: 'p', c7: 'p', d7: 'p', e7: 'q', f7: 'p',
-  g7: 'b',
-  a6: 'b',
-  b6: 'n',
-  e6: 'p',
-  f6: 'n',
-  g6: 'p',
-  d5: 'P',
-  e5: 'N',
-  b4: 'p',
-  e4: 'P',
-  c3: 'N',
-  f3: 'Q',
-  h3: 'p',
-  a2: 'P',
-  b2: 'P',
-  c2: 'P',
-  d2: 'B',
-  e2: 'B',
-  f2: 'P',
-  g2: 'P',
-  h2: 'P',
-  a1: 'R',
-  e1: 'K',
-  h1: 'R'
+  a8: 'r', e8: 'k',  h8: 'r',
+  a7: 'p', c7: 'p', d7: 'p', e7: 'q', f7: 'p', g7: 'b',
+  a6: 'b', b6: 'n', e6: 'p', f6: 'n', g6: 'p',
+  d5: 'P', e5: 'N',
+  b4: 'p', e4: 'P',
+  c3: 'N', f3: 'Q', h3: 'p',
+  a2: 'P', b2: 'P', c2: 'P', d2: 'B', e2: 'B', f2: 'P',  g2: 'P',  h2: 'P',
+  a1: 'R', e1: 'K', h1: 'R'
 });
 
 function executeMoves(moves) {
